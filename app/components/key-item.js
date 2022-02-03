7d206a37832d6a6b036262d1079917cd845c93c8
Alyssa Gardiner <alyssa.gardiner@iorahealth.com>
2022-02-03 13:05:53 -0500

    Adding color picker and edit buttons to key

--- app/components/key-item.js
+++ app/components/key-item.js
@@ -1,7 +1,23 @@
 import Component from '@glimmer/component';
+import { action, set } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class KeyItemComponent extends Component {
   @tracked showModal = false;
+
+  @action
+  onInit(pickrInstance) {
+      pickrInstance.setColor(this.args.keyItem.color);
+    }
+
+  @action
+  handleOnChange(hsva, pickrInstance) {
+      if(hsva) {
+//        TODO: pass in the method so the updated color is higher up
+        set(this.args.keyItem, 'color', hsva.toHEXA().toString());
+      }
+      pickrInstance.hide()
+    }
+
 
 }
