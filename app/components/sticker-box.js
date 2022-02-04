b90dce5589c7938f2ce2a629b6cc20fb464e255a
Laura Kajpust <laura.kajpust@iorahealth.com>
2022-02-04 14:00:49 -0600

    Add more stickers

--- app/components/sticker-box.js
+++ app/components/sticker-box.js
@@ -7,5 +7,15 @@ export default class StickerBoxComponent extends Component {
     { id: 3, name: 'Flex', img: '💪' },
     { id: 4, name: 'Trophy', img: '🏆' },
     { id: 5, name: 'Triumph', img: '😤' },
+    { id: 6, name: 'Sob', img: '😭' },
+    { id: 7, name: 'Sparkling Heart', img: '💖' },
+    { id: 8, name: 'Soccer Ball', img: '⚽' },
+    { id: 9, name: 'Bike', img: '🚲' },
+    { id: 10, name: 'Sparkles', img: '✨' },
+    { id: 11, name: 'Bear', img: '🐻' },
+    { id: 12, name: 'Tomato', img: '🍅' },
+    { id: 13, name: 'Taco', img: '🌮' },
+    { id: 14, name: 'Yoga', img: '🧘' },
+    { id: 15, name: 'Boom', img: '💥' },
   ];
 }
