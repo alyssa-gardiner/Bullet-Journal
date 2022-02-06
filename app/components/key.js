dd9051988d77a42ca380001ebe7af6835d0bca2d
Alyssa Gardiner <alyssa.gardiner@iorahealth.com>
2022-02-06 16:54:29 -0500

    Adding an add exercise button

--- app/components/key.js
+++ app/components/key.js
@@ -7,13 +7,34 @@ export default class KeyComponent extends Component {
   @service store;
 
   @tracked keyItems = this.store.findAll('exercise');
+  @tracked showAddModal = false;
+  @tracked newName = '';
+  @tracked color = null;
 
   @action
   onColorChange(keyItemName, newColor, keyItemId) {
+    this.store.findRecord('exercise', keyItemId).then(function (exercise) {
+      exercise.color = newColor;
+      exercise.save();
+    });
+  }
 
-  this.store.findRecord('exercise', keyItemId).then(function(exercise) {
-    exercise.color = newColor;
-    exercise.save();
-  });
+  @action
+  handleOnChange(hsva, pickrInstance) {
+    if (hsva) {
+      this.color = hsva.toHEXA().toString();
+    }
+    pickrInstance.hide();
+  }
+
+  @action
+  addExercise() {
+    let newExercise = this.store.createRecord('exercise', {
+      name: this.newName,
+      color: this.color,
+    });
+    newExercise.save();
+    console.log('yes the save button has color', this.color);
+    console.log('yes the save button has new name', this.newName);
   }
 }
