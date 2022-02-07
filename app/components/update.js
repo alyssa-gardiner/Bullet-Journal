fe5fc0c57624d58875e6758ea19dbbf3ca9e3dbe
Alyssa Gardiner <alyssa.gardiner@iorahealth.com>
2022-02-06 20:18:54 -0500

    Adding day

--- app/components/update.js
+++ app/components/update.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { action, set } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
+import { Promise as EmberPromise } from 'rsvp';
 
 export default class UpdateComponent extends Component {
   @service store;
@@ -9,8 +10,24 @@ export default class UpdateComponent extends Component {
   @tracked keyItems = this.store.findAll('exercise');
 
   @action
-  updateDay(keyItem) {
-    set(this.args.day, 'exercise', keyItem);
+  async updateDay(keyItem) {
+    let matchingDay = await this.store.queryRecord('day', {dayOfMonth: this.args.day.number,monthNumber: this.args.day.monthNumber})
+    .catch(err => {
+        if(err.errors.length < 2 && err.errors[0].status == 404) {
+          let newDay = this.store.createRecord('day', {
+          monthNumber: this.args.day.monthNumber,
+          dayOfMonth: this.args.day.number,
+          exerciseId: keyItem.id
+          });
+          debugger;
+          newDay.save();
+        }});
+    if(matchingDay) {
+    debugger;
+    this.store.findRecord('day', matchingDay.id).then(function(day) {
+      day.exerciseId = keyItem.id;
+    });
+    }
   }
 
   @action
--- app/routes/tracker.js
+++ app/routes/tracker.js
@@ -1,50 +1,46 @@
 import Route from '@ember/routing/route';
 
 export default class TrackerRoute extends Route {
-  model() {
+   async model() {
     let months = [
-      { name: 'January', dayCount: 31, days: [] },
-      { name: 'February', dayCount: 28, days: [] },
-      { name: 'March', dayCount: 31, days: [] },
-      { name: 'April', dayCount: 30, days: [] },
-      { name: 'May', dayCount: 31, days: [] },
-      { name: 'June', dayCount: 30, days: [] },
-      { name: 'July', dayCount: 31, days: [] },
-      { name: 'August', dayCount: 31, days: [] },
-      { name: 'September', dayCount: 30, days: [] },
-      { name: 'October', dayCount: 31, days: [] },
-      { name: 'November', dayCount: 30, days: [] },
-      { name: 'December', dayCount: 31, days: [] },
+      { number: 1, name: 'January', dayCount: 31, days: [] },
+      { number: 2, name: 'February', dayCount: 28, days: [] },
+      { number: 3, name: 'March', dayCount: 31, days: [] },
+      { number: 4, name: 'April', dayCount: 30, days: [] },
+      { number: 5, name: 'May', dayCount: 31, days: [] },
+      { number: 6, name: 'June', dayCount: 30, days: [] },
+      { number: 7, name: 'July', dayCount: 31, days: [] },
+      { number: 8, name: 'August', dayCount: 31, days: [] },
+      { number: 9, name: 'September', dayCount: 30, days: [] },
+      { number: 10, name: 'October', dayCount: 31, days: [] },
+      { number: 11, name: 'November', dayCount: 30, days: [] },
+      { number: 12, name: 'December', dayCount: 31, days: [] },
     ];
-    months.forEach((month) => {
-      let days = [];
-      let dayNumbers = Array.from({ length: month.dayCount }, (v, i) => i + 1);
-      dayNumbers.forEach((dayNumber) => {
-        let day = {
-          month: month.name,
-          number: dayNumber,
-          exercise: null,
-          description: '',
-        };
-        days.push(day);
-      });
-      month.days = days;
-    });
-    return months;
-  }
-}
 
-[
-  { name: 'January', dayCount: 31 },
-  { name: 'February', dayCount: 28 },
-  { name: 'March', dayCount: 31 },
-  { name: 'April', dayCount: 30 },
-  { name: 'May', dayCount: 31 },
-  { name: 'June', dayCount: 30 },
-  { name: 'July', dayCount: 31 },
-  { name: 'August', dayCount: 31 },
-  { name: 'September', dayCount: 30 },
-  { name: 'October', dayCount: 31 },
-  { name: 'November', dayCount: 30 },
-  { name: 'December', dayCount: 31 },
-];
+    for (const month of months) {
+      let days = [];
+      let responseError = false;
+      let dayNumbers = Array.from({ length: month.dayCount }, (v, i) => i + 1);
+      for (const dayNumber of dayNumbers) {
+        let matchingDay = await this.store.queryRecord('day', {dayOfMonth: dayNumber,monthNumber: month.number})
+        .catch(err => {if(err.errors.length < 2 && err.errors[0].status == 404) {
+          responseError = true;
+        }});
+        if(responseError) {
+            //Push a fake day in
+          let day = {
+            monthNumber: month.number,
+            dayOfMonth: dayNumber,
+            exerciseId: null,
+          };
+          days.push(day);
+        } else {
+          //push the existing day in
+          days.push(matchingDay);
+        }
+      }
+      month.days = days;
+    }
+    return months;
+   }
+}
